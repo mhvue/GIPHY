d859e854a970e59f0dca50c016d85b6ad89953b7
Mai Houa Vue <mhvue4@gmail.com>
2020-01-12 15:54:09 -0600

    added intital ajax

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -1,5 +1,5 @@
 //array of strings 
-var topics = ["Taylor Swift",  "Beyonce", "Celine Dione", "Christina Aguilera", "Katy Perry", "Lady Gaga", "Brittney Spears", "Rhianna", 
+var topics = ["taylorswift",  "Beyonce", "Celine Dione", "Christina Aguilera", "Katy Perry", "Lady Gaga", "Brittney Spears", "Rhianna", 
 "Ariana Grande","Adele"];
 
 singerButtons();
@@ -10,11 +10,27 @@ function singerButtons () {
     for (var i = 0; i < topics.length; i++) {
         var buttons = $("<button>" + "<br>").addClass("femaleArtist").attr("data-name", topics[i]).text(topics[i]);
         
-       // $(".singerButtons").html(" ");
         $(".singerButtons").append(buttons);
 
         //console.log(topics[i]);
         }
        
 };
+
+
+
+var API= "0LXaBTUDz62eiqycLJFendA0KcZxuHfR"
+var queryURL= "https://api.giphy.com/v1/gifs/search?api_key=" + API
+ + "&q="+ topics[0]+ "&limit=10&offset=0&rating=G&lang=en";
+
+
+//create a on click event to pick an item from array for ajax to call
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+});
+
 
